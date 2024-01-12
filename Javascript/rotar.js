1048fee30c9cb3c486813f70c1e28641e57b2b68
Sergio Dimas <152244192+SergioDimas17@users.noreply.github.com>
2024-01-12 14:14:34 -0500

    Add files via upload

--- Javascript/rotar.js
+++ Javascript/rotar.js
@@ -1,8 +1,10 @@
+// Función para rotar la imagen y mostrar el overlay al pasar el ratón sobre el servicio
 function rotateImage(element) {
     element.querySelector('.servicio img').classList.add('rotate');
     element.querySelector('.overlay').style.opacity = 1; /* Muestra el overlay */
 }
 
+// Función para restablecer la imagen y ocultar el overlay al quitar el ratón del servicio
 function resetImage(element) {
     element.querySelector('.servicio img').classList.remove('rotate');
     element.querySelector('.overlay').style.opacity = 0; /* Oculta el overlay */
